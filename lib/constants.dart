7b22f6272ac7ba70b7f825f1708a23a007ab7c80
Roni Ghosh <theronighosh@gmail.com>
2024-12-12 18:47:31 +0530

    https

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,2 @@
 // const baseUrl = "https://cors-anywhere.herokuapp.com/https://hmroyal.online/hmroyal/api";
-const baseUrl = "http://13.202.13.180/api";
+const baseUrl = "https://13.202.13.180/api";
